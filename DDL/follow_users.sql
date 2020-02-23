5e5f6e6073de501e934f0756dfbcd5f21e3b826d
toriyab <toriyabepunipuni@gmail.com>
2020-02-23 16:59:18 +0900

    modify follow_users table column name

--- DDL/follow_users.sql
+++ DDL/follow_users.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `follow_users` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `from` int NOT NULL,
-  `to` int NOT NULL,
+  `from_user` int NOT NULL,
+  `to_user` int NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE = InnoDB DEFAULT CHARSET = utf8;
 
